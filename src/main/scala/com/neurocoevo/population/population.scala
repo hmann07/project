d34382480f25cbe59943863b6fbf5e9a053a6293
Henry G. Mann <henrygmann@hotmail.co.uk>
2017-07-12 22:42:27 +0100

    crossover

--- src/main/scala/com/neurocoevo/population/population.scala
+++ src/main/scala/com/neurocoevo/population/population.scala
@@ -61,6 +61,12 @@ import Population._
 	// CROSSOVER
 	// we could move this to the agents... or perhaps pass it as a function to them...
 	// benefit being it would get done in parallel in large populations this matters.
+	// crossover in NEAT actually refers specifically to the connections. Though the Neruons are obviously involved...
+	// the neurons for the new genome can be retrieved by a pass through the new connection genome.
+	// but which do we take?? In general the Neruons wiht the same innovation number should both be the same
+	// however I have added Bias to the neuron itself meaning during learning/mutation they will diverge. Equally perhaps i tis interesting
+	// to include mutation of the activation function. Could take neuron
+	// randomly or that of the fittest.  
 
 	def crossover(g: List[AgentResults]): NetworkGenome = {
 
@@ -68,19 +74,24 @@ import Population._
 		val networkGenome1 = g(0).crossOverGenomes // Due to the sorting and how the partition data will work. this will always be the strongest.
 		val networkGenome2 = g(1).crossOverGenomes
 
-		val crossedNeurons: List[NeuronGenome] = networkGenome1.neurons.map { n => 
-			val matching = networkGenome2.neurons.find(n2 => n2.innovationId == n.innovationId)
+		val crossedConnections: List[ConnectionGenome] = networkGenome1.connections.map { n => 
+			val matching = networkGenome2.connections.find(n2 => n2.innovationId == n.innovationId)
 			if (matching.get != None) {
-				List(n, matching.get)(Random.nextInt(1))
+				// Randomly take one or other of the genomes.
+				List(n, matching.get)(Random.nextInt(2))
 			} else {
+				// This is the stronger genome take its additional parts. discard the other.
+				// TODO: Do we not even want to consider the weaker disjoints or excesss genes. in sharpNeat this appears to be toggled
+				// at one point even randomly..
 				n
 			}
 			}.toList
 		
-		println(crossedNeurons)
+		val newGenomes = crossedConnections.map(c => Map(c.from -> networkGenome1.neurons.find(n => n.innovationId == c.from)))
+		println(newGenomes)
 
 
-		new NetworkGenome(crossedNeurons, networkGenome2.connections)
+		new NetworkGenome(networkGenome1.neurons, crossedConnections)
 
 	}
 
