614bca752864a52b0be32e9ebf3f96d53f584f47
hmann07 <hmann07@dcs.bbk.ac.uk>
2017-07-13 16:54:37 +0100

    HashMap

--- src/main/scala/com/neurocoevo/genome/genomeFactory.scala
+++ src/main/scala/com/neurocoevo/genome/genomeFactory.scala
@@ -5,19 +5,23 @@ import scala.xml.XML
 import scala.util.Random
 
 import com.neurocoevo.substrate._
+import scala.collection.immutable.HashMap
 
 
-// SHould be able to create a network Genome based on a number of options... 
+// SHould be able to create a network Genome based on a number of options...
 // xml file, or, layer definition
 // Most likely going to be called by the agent.
 
 object GenomeFactory {
-	
+
 	def createGenome(filepath: String): NetworkGenome = {
 
 		val xmlData = XML.loadFile(filepath)
-		 
-		val neurons: List[NeuronGenome] = (xmlData \ "neurons" \ "neuron").map {i => 
+
+		val neurons: HashMap[Int, NeuronGenome] = (xmlData \ "neurons" \ "neuron").foldLeft(HashMap[Int, NeuronGenome]()) {(m, i) =>
+
+				m + ((i \ "@id").text.toInt ->
+
 				new NeuronGenome( (i \ "@id").text.toInt,
 								  (i \ "@activationFuntion").text,
 								  (i \ "@type").text,
@@ -27,11 +31,14 @@ object GenomeFactory {
 								  	if(x.length == 0){
 								  		Random.nextDouble
 								  	} else {
-								  		x.toDouble 
+								  		x.toDouble
 								  	}
-								  })} .toList
+								  }))}
 
-		val connections: List[ConnectionGenome] = (xmlData \ "connections" \ "connection").map {i => 
+		val connections: HashMap[Int, ConnectionGenome] = (xmlData \ "connections" \ "connection").foldLeft(HashMap[Int, ConnectionGenome]()) {(m, i) =>
+
+				m + ((i \ "@id").text.toInt ->
+
 				new ConnectionGenome( (i \ "@id").text.toInt,
 								  (i \ "@src-id").text.toInt,
 								  (i \ "@tgt-id").text.toInt,
@@ -40,14 +47,14 @@ object GenomeFactory {
 								  	if(x.length == 0){
 								  		Random.nextDouble
 								  	} else {
-								  		x.toDouble 
+								  		x.toDouble
 								  	}
-								  })} .toList
-	
-		
+								  }))}
+
+
 		new NetworkGenome(neurons, connections)
 
-		
+
 	}
 
 	//def createGenome(substrate: Substrate, cppn: NetworkGenome): NetworkGenome = {}
@@ -55,14 +62,14 @@ object GenomeFactory {
 
 
 	// TODO: Does not currently provide correct Id's for the Neurons
-
+	/*
 	def createGenome(inputs: Int, hidden: List[Int], outputs: Int): NetworkGenome = {
-		
-		val inputList: List[NeuronGenome] = 1.to(inputs).toList.map(i => new NeuronGenome(i, "Sigmoid", "input", 0, 0))
-		val hiddenList: List[List[NeuronGenome]] = hidden.map(l => 1.to(l).toList.map(l => new NeuronGenome(l, "Sigmoid", "hidden", -1, Random.nextDouble) ))
-		val outputList: List[NeuronGenome] = 1.to(outputs).toList.map(o => new NeuronGenome(o, "Sigmoid", "output", -1, Random.nextDouble))
-		
-		val combinedNeuronList: List[List[NeuronGenome]] = inputList :: hiddenList.flatten :: outputList :: List.empty
+
+		val inputList: HashMap[Int, NeuronGenome] = 1.to(inputs).toList.foldLeft(Map[Int, ConnectionGenome]()) { (d,i) => d + (i -> new NeuronGenome(i, "Sigmoid", "input", 0, 0)) }
+		val hiddenList: List[HashMap[Int, NeuronGenome]] = hidden.map(l => 1.to(l).toList.foldLeft(Map[Int, ConnectionGenome]()) { (d,i) => d + (i ->  new NeuronGenome(i, "Sigmoid", "hidden", -1, Random.nextDouble))})
+		val outputList: HashMap[Int, NeuronGenome] = 1.to(outputs).toList.foldLeft(Map[Int, ConnectionGenome]()) { (d,i) => d + (i -> new NeuronGenome(o, "Sigmoid", "output", -1, Random.nextDouble)) }
+
+		val combinedNeuronList: List[Map[Int, NeuronGenome]] = inputList :: hiddenList.flatten :: outputList :: List.empty
 		val connections = connectionGen(combinedNeuronList)
 		new NetworkGenome(combinedNeuronList.flatten, connections)
 	}
@@ -73,19 +80,19 @@ object GenomeFactory {
 	}
 
 	private def connectionGenAux(
-		from: List[NeuronGenome], 
-		to: List[NeuronGenome], 
-		connections: List[ConnectionGenome], 
+		from: List[NeuronGenome],
+		to: List[NeuronGenome],
+		connections: List[ConnectionGenome],
 		staticOutputs: List[NeuronGenome],
 		toConnect: List[List[NeuronGenome]]): List[ConnectionGenome] = {
-			
+
 		if(from.length == 0 && toConnect.length == 0 ){
 			// we've iterated through the whole input list. and there're no subsequent layers to connect
-			connections		
+			connections
 		} else {
 			if(from.length == 0 && toConnect.length > 0 ){
 				// made all connections for this layer, but there is still more layers to connect, move to the next layer.
-				connectionGenAux(staticOutputs, toConnect.head, connections, toConnect.head, toConnect.tail)	
+				connectionGenAux(staticOutputs, toConnect.head, connections, toConnect.head, toConnect.tail)
 			} else {
 				if(to.length > 0){
 					// add a connection from first input to current first in the output list, go to next output.
@@ -97,5 +104,5 @@ object GenomeFactory {
 			}
 		}
 	}
-
+ */
 }
