8a946a9259e11d0320cbbfc09937b28ba9d1aeee
hmann07 <hmann07@dcs.bbk.ac.uk>
2017-07-13 16:54:01 +0100

    HashMap

--- src/main/scala/com/neurocoevo/genome/networkGenome.scala
+++ src/main/scala/com/neurocoevo/genome/networkGenome.scala
@@ -1,8 +1,10 @@
 package com.neurocoevo.genome
 
-case class NetworkGenome(val neurons: List[NeuronGenome],
-					val connections: List[ConnectionGenome]){
-	val inputNodes: List[NeuronGenome] = neurons.filter(n => n.neuronType == "input")
-	val outputNodes: List[NeuronGenome] = neurons.filter(n => n.neuronType == "output")
-	val hiddenNodes: List[NeuronGenome] = neurons.filter(n => n.neuronType == "hidden")
+import scala.collection.immutable.HashMap
+
+case class NetworkGenome(val neurons: HashMap[Int, NeuronGenome],
+					val connections: HashMap[Int, ConnectionGenome]){
+	val inputNodes: HashMap[Int, NeuronGenome] = neurons.filter(n => n._2.neuronType == "input")
+	val outputNodes: HashMap[Int, NeuronGenome] = neurons.filter(n => n._2.neuronType == "output")
+	val hiddenNodes: HashMap[Int, NeuronGenome] = neurons.filter(n => n._2.neuronType == "hidden")
 }
