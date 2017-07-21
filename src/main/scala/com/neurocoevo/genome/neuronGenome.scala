1aa89fe767dd9522f1afe104e8b897928132c1a8
hmann07 <hmann07@dcs.bbk.ac.uk>
2017-07-21 15:57:28 +0100

    layers

--- src/main/scala/com/neurocoevo/genome/neuronGenome.scala
+++ src/main/scala/com/neurocoevo/genome/neuronGenome.scala
@@ -2,6 +2,11 @@ package com.neurocoevo.genome
 
 import com.neurocoevo.neuron._
 
-class NeuronGenome(val innovationId: Int , val activationFunction: String, val neuronType: String, val bias: Int = 0, val biasWeight: Double){
+class NeuronGenome(val innovationId: Int , 
+                   val activationFunction: String, 
+                   val neuronType: String, 
+                   val bias: Int = 0, 
+                   val biasWeight: Double
+                   val layer: Double = 0){
 
 }
