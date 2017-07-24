1851d8d5a3b685dbee246ccb2949b9ef8359b9a1
hmann07 <hmann07@dcs.bbk.ac.uk>
2017-07-24 19:49:02 +0100

    genetic mutations

--- src/main/scala/com/neurocoevo/genome/connectionGenome.scala
+++ src/main/scala/com/neurocoevo/genome/connectionGenome.scala
@@ -3,9 +3,9 @@ package com.neurocoevo.genome
 import scala.util.Random
 
 case class ConnectionGenome(
-	val innovationId: Int, 
-	val from: Int, 
-	val to: Int, 
-	val weight: Double = Random.nextDouble(), 
-	val enabled: Boolean = true)
-
+	val innovationId: Int,
+	val from: Int,
+	val to: Int,
+	val weight: Double = Random.nextDouble(),
+	val enabled: Boolean = true,
+	val recurrent: Boolean = false)
