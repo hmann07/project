3e916b65c81d952392a9b0060f38cd7f4f0c45f7
hmann07 <hmann07@dcs.bbk.ac.uk>
2017-07-25 17:57:11 +0100

    Update connectionGenome.scala

--- src/main/scala/com/neurocoevo/genome/connectionGenome.scala
+++ src/main/scala/com/neurocoevo/genome/connectionGenome.scala
@@ -6,6 +6,6 @@ case class ConnectionGenome(
 	val innovationId: Int,
 	val from: Int,
 	val to: Int,
-	val weight: Double = Random.nextDouble(),
+	val weight: Double = (Random.nextDouble * 2) - 1,
 	val enabled: Boolean = true,
 	val recurrent: Boolean = false)
