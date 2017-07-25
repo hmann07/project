4d4e1b197559a07c37d75de57bd8d716187f93dd
hmann07 <hmann07@dcs.bbk.ac.uk>
2017-07-25 17:55:56 +0100

    Update neuronGenome.scala

--- src/main/scala/com/neurocoevo/genome/neuronGenome.scala
+++ src/main/scala/com/neurocoevo/genome/neuronGenome.scala
@@ -1,12 +1,13 @@
 package com.neurocoevo.genome
 
 import com.neurocoevo.neuron._
+import scala.util.Random
 
-class NeuronGenome(val innovationId: Int , 
-                   val activationFunction: String, 
-                   val neuronType: String, 
-                   val bias: Int = 0, 
-                   val biasWeight: Double,
+case class NeuronGenome(val innovationId: Int ,
+                   val activationFunction: String,
+                   val neuronType: String,
+                   val bias: Int = 0,
+                   val biasWeight: Double = (Random.nextDouble * 2) - 1,
                    val layer: Double = 0){
 
 }
