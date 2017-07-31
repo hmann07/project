33c1eb7cacb8152b3794d216eba7540bfa4b6368
hmann07 <hmann07@dcs.bbk.ac.uk>
2017-07-31 17:51:30 +0100

    Update species.scala

--- src/main/scala/com/neurocoevo/speciation/species.scala
+++ src/main/scala/com/neurocoevo/speciation/species.scala
@@ -4,5 +4,6 @@ import com.neurocoevo.genome.NetworkGenome
 
 case class Species(
 	val champion: NetworkGenome = null,
-	val members: List[NetworkGenome] = null
+	val members: List[NetworkGenome] = null,
+	val speciesTotalFitness: Double = 0
 )
