1a55d85268d2f463752ac5b58634bdca81fa0090
hmann07 <hmann07@dcs.bbk.ac.uk>
2017-08-01 14:35:11 +0100

    Update species.scala

--- src/main/scala/com/neurocoevo/speciation/species.scala
+++ src/main/scala/com/neurocoevo/speciation/species.scala
@@ -1,10 +1,11 @@
 package com.neurocoevo.speciation
 
 import com.neurocoevo.genome.NetworkGenome
+import scala.collection.immutable.HashMap
 
 case class Species(
 	val champion: NetworkGenome = null,
-	val members: List[NetworkGenome] = null,
+	val members: HashMap[Int, SpeciesMember] = null,
 	val membersCount: Int = 0, // May as well calc here to avoid calling length on the list all the time.
 	val speciesTotalFitness: Double = 0,
 	val speciesMeanFitness: Double = 0,
