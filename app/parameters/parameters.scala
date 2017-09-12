e48a042f43307c6a15b2169184c73cf47095ecf6
henry <henrygmann@hotmail.co.uk>
2017-09-12 23:56:02 +0100

    find hyperNEAT parameters

--- app/parameters/parameters.scala
+++ app/parameters/parameters.scala
@@ -3,12 +3,12 @@ package com.neurocoevo.parameters
 	case class SpeciationParameters(
 		val c1: Double = 1.0,
 		val c2: Double = 1.0,
-		val c3: Double = 0.4,
-		val speciationThreshold: Double = 3.0)
+		val c3: Double = 0.3,
+		val speciationThreshold: Double = 2.0)
 
 
 	case class PopulationParameters(
-		val populationSize: Int = 150,
+		val populationSize: Int = 100,
 		val genomePath: String = ".\\resources\\cppnSubstrate.xml", // "cppnSubstrate.xml" or annSubstrate.xml or annBPSubstrate.xml
 		val agentType: String = "HYPER", // Options: HYPER, STD, BP
 		val altGenomePath: String = ".\\resources\\hyperneatAnnSubstrate.xml"
@@ -22,19 +22,19 @@ package com.neurocoevo.parameters
 	case class OffspringParameters(
 		val crossoverRate: Double = 0.25,
 		val mutationRate: Double = 0.75,
-		val elitismRate: Double = 0.2
+		val elitismRate: Double = 0.1
 	)
 
 
 	case class MutationFunctionParameters (
 	// Should add to 1.
-		val perturbWeightRate: Double = 0.92,
-		val addNeuronRate: Double = 0.03,
-		val addConnectionRate: Double = 0.05,
+		val perturbWeightRate: Double = 0.8,
+		val addNeuronRate: Double = 0.1,
+		val addConnectionRate: Double = 0.1,
 	//
-		val weightChangeProportion: Double = 0.8,
-		val jiggleProportion: Double = 0.9,
-		val connectionWeightRange: Double = 10.0,
+		val weightChangeProportion: Double = 0.6,
+		val jiggleProportion: Double = 0.7, // non-jiggle are reset
+		val connectionWeightRange: Double = 8.0,
 		val mutationPertubFactor: Double = 0.05
 	)
 
@@ -45,4 +45,4 @@ package com.neurocoevo.parameters
 		)
 
 	case class UniverseParameters (
-		val populationCount: Int = 20)
+		val populationCount: Int = 1)
