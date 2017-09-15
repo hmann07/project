26e77913696282197d6eced6cb79aa061be2b926
henry <henrygmann@hotmail.co.uk>
2017-09-15 21:25:55 +0100

    Visual Discrimination exp

--- app/evolution/VDevaluator.scala
+++ app/evolution/VDevaluator.scala
@@ -12,17 +12,32 @@ case class VDEvaluator(
 	
 	override def evaluateIteration(networkInput: Sensation, networkOutput: SortedMap[Int, Double]): Evaluator = {
 		  
-  		  val error = networkInput.label(0) - networkOutput(3)
-  		  val squaredError = math.pow(error, 2)
+
+		  val x1 = networkInput.label(0)
+		  val y1 = networkInput.label(1)
+  		  
+  		  // First workout the output that gave the max.
+  		  val maxNeuron = networkOutput.maxBy(m => m._2) 
+		 
+  		  val x2 = maxNeuron._1 - 1 % 11
+		  val y2 = math.floor(maxNeuron._1 - 1 / 11)
+
+  		  // Then work out the distance of that output from the true output
           
-          this.copy(aggregatedIterationValue = aggregatedIterationValue + squaredError)
+  		  val d = Math.sqrt(Math.pow(x1 - x2,2) + Math.pow(y1 - y2, 2))
+
+          this.copy(aggregatedIterationValue = aggregatedIterationValue + d)
 	}
 
 
 	override def evaluateEpoch() = {
 		
-		val evalfitness = 1 / aggregatedIterationValue
+		  // we need to inverse distnace since the smaller the better
+
+		  var invD = 1 / aggregatedIterationValue
 		
-		this.copy(fitness = evalfitness, auxValue = aggregatedIterationValue)
+			// Aux value is the average distance... 
+
+		this.copy(fitness = invD, auxValue = aggregatedIterationValue / 75)
 	}
 } 
---
+++ app/experience/experience - Xor
@@ -0,0 +1,37 @@
+package com.neurocoevo.experience
+
+import akka.actor.ActorSystem
+import akka.actor.{Actor, ActorRef, ActorLogging, Props, Inbox}
+
+object Experience {
+  
+  	case class Event(event: List[Double], meaning: List[Double]) 
+
+  	val events: List[Event] = List(
+  		Event(List(1, 1), List(0)),
+  		Event(List(0, 1), List(1)),
+  		Event(List(1, 0), List(1)),
+  		Event(List(0, 0), List(0)))
+
+  	case class ExperienceSettings(experiencesServed: Int = 0)
+
+}
+
+
+class Experience extends Actor with ActorLogging {
+	import Experience._
+	import context._
+
+	def receive =  servePerceptions(ExperienceSettings())
+
+	def servePerceptions(s: ExperienceSettings): Receive = {
+		
+		case "perceive" =>
+			//println("experience #" + (s.experiencesServed + 1) + events(s.experiencesServed % (events.length)))
+			sender ! events(s.experiencesServed % (events.length))
+			context become servePerceptions(s.copy(experiencesServed = s.experiencesServed + 1))
+
+		case "STOP" =>
+			context stop self
+	}
+}
